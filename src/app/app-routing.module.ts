1c78985c0e988a7d6ad761237fb88133c1ae0f55
Leo-Kopec <lfkopec@loyola,edu>
2022-08-01 16:56:30 -0400

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FindByEmalComponent } from './components/find-by-email/find-by-email.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'reservations',
+  component: FindByEmalComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
