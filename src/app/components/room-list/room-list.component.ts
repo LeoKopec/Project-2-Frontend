a057af0ff58c5a85daad2e5f0da5e3e2688ecd21
Derek Enlow <theunnamed97@gmail.com>
2022-08-04 14:36:06 -0600

    added redirect with needed params

--- src/app/components/room-list/room-list.component.ts
+++ src/app/components/room-list/room-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Price } from 'src/app/models/price.model';
 import { Room } from 'src/app/models/room.model';
 
@@ -11,13 +12,24 @@ export class RoomListComponent implements OnInit {
 
   @Input() rooms: Array<Room> = [];
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
   totalPrice(prices: Array<number>): number {
     return prices.reduce((prev, curr) => prev + curr);
+  }
+
+  onRoomReservation(room: Room) {
+    let queryParams: {[x: string]: unknown} = {room: room.id}
+    let currParams = this.route.snapshot.queryParamMap;
+    for (const prop of ['start', 'end', 'hotel']) {
+      if (currParams.has(prop)) {
+        queryParams[prop] = currParams.get(prop);
+      }
+    }
+    this.router.navigate(['/book-reservation'], {queryParams: queryParams });
   }
 
 }
