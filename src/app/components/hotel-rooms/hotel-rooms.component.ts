f8d497a02bf6f4b4f8551d199c973f663d0606a9
Derek Enlow <theunnamed97@gmail.com>
2022-08-03 21:25:41 -0600

    added hotel header and lightly formatted

--- src/app/components/hotel-rooms/hotel-rooms.component.ts
+++ src/app/components/hotel-rooms/hotel-rooms.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Hotel } from 'src/app/models/hotel.model';
 import { Room } from 'src/app/models/room.model';
+import { HotelService } from 'src/app/services/hotel.service';
 import { RoomService } from 'src/app/services/room.service';
+import { roundRating } from 'src/app/services/value-adjustment';
 
 @Component({
   selector: 'app-hotel-rooms',
@@ -10,14 +13,26 @@ import { RoomService } from 'src/app/services/room.service';
 })
 export class HotelRoomsComponent implements OnInit {
 
+  hotel!: Hotel;
+  roundedRating: number = 0;
   rooms: Array<Room> = [];
 
-  constructor(private router: Router, private route: ActivatedRoute, private roomService: RoomService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private roomService: RoomService,
+    private hotelService: HotelService
+    ) { }
 
   ngOnInit(): void {
     // Check for room search parameters
     let params = this.route.snapshot.queryParamMap;
     if (params.has('start') && params.has('end') && params.has('hotel') && params.has('size')) {
+      // Retrieve the hotel
+      this.hotelService.findById(parseInt(params.get('hotel') as string)).subscribe(data => {
+        this.hotel = data;
+        this.roundedRating = this.hotel.rate;
+      });
       // Retrieve the list of hotels
       this.roomService.findByParams(
         parseInt(params.get('hotel') as string),
